c7d43fe2e9705262429c362e03603a27dace2a74
Daniel Ørsted <>
2025-07-17 16:10:20 +0200

    base updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // eslint-disable-next-line import/no-unresolved
 import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        'crypto',
+        'util',
+        'stream',
+      ]
+    }
+  },
   plugins: [
+    nodePolyfills({
+      include: [
+        'crypto',
+        'util',
+        'stream',
+      ],
+      protocolImports: true,
+    }),
     Unfonts({
       custom: {
         display: 'auto',
