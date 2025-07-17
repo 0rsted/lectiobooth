504ab7e1e9e6ba1063e0f67f80dc605753c7e6dd
Daniel Ørsted <>
2025-07-17 16:20:21 +0200

    make the lectioApi available

--- src/preload.ts
+++ src/preload.ts
@@ -2,7 +2,7 @@
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 
 import { contextBridge, ipcRenderer, session, app } from 'electron'
-import { getAllSchools } from './functions/getAllSchools'
+import * as lectioApi from './lectioApi'
 
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
@@ -14,4 +14,8 @@ contextBridge.exposeInMainWorld('versions', {
 contextBridge.exposeInMainWorld('electronAPI', {
   setTitle: (title: string) => ipcRenderer.send('set-title', title),
   app
+})
+
+contextBridge.exposeInMainWorld('lectioApi', {
+  ...lectioApi
 })
