15c5794b396220ff78ca22eac38f93ecaa593522
Daniel Ørsted <>
2025-07-23 16:11:16 +0200

    clean up the code

--- src/components/label.ts
+++ src/components/label.ts
@@ -1,6 +1,6 @@
 export const label = (forId: string, content: string) => {
   const element = document.createElement('label')
   element.setAttribute('for', forId)
-  element.textContent = content
+  element.innerText = content
   return element
 }
