39faa5d82b833b31dc116737dbb830388099328a
Daniel Ørsted <>
2025-07-23 16:01:48 +0200

    cleaning

--- src/functions/clearChildren.ts
+++ src/functions/clearChildren.ts
@@ -1,14 +1,6 @@
-export const clearChildren = (element: HTMLElement, hard = false) => {
+export const clearChildren = (element: HTMLElement) => {
   element.textContent = ""
   return
-
-  if (hard)
-    while (element.hasChildNodes())
-      element.childNodes.forEach(child => document.body.removeChild(child))
-  else
-    for (let child of element.children) {
-      element.removeChild(child)
-    }
 }
 
 export default clearChildren
