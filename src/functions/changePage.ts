1c26eda9dcab80734dedf1b46b5e16ef0cc31347
Daniel Ørsted <>
2025-07-23 16:04:41 +0200

    updated pageChange handler

--- src/functions/changePage.ts
+++ src/functions/changePage.ts
@@ -1,1 +1,51 @@
-export const changePage = (pageName: string) => window.dispatchEvent(new CustomEvent('changePage', { detail: {pageName} }))
+import { clearChildren } from "./clearChildren"
+
+export type PageFunctions = {
+  id: string
+  renderer: () => any
+  unRender?: VoidFunction
+}
+
+export class PageManager {
+  #currentPage?: PageFunctions = {
+    id: '',
+    renderer: () => {/* */}
+  }
+  #window: Window
+  #baseElement?: HTMLElement
+  #debug: boolean
+
+  constructor(window: Window, baseElement: HTMLElement, debug = false) {
+    this.#window = window
+    this.#debug = debug
+    this.#baseElement = baseElement
+    window.addEventListener('changePage', (evt: ChangePage) => this.updatePage(evt))
+  }
+
+  private updatePage({pageFunctions: pageFunctions}: ChangePage) {
+    this.#debug && console.log('pageTest', this.#currentPage.id, pageFunctions.id)
+    if(pageFunctions.id === this.#currentPage.id)
+      return
+    this.#debug && console.log('updatePage', this.#currentPage, pageFunctions)
+    if(this.#currentPage.unRender) {
+      this.#debug && console.log('unRender')
+      this.#currentPage.unRender()
+    }
+    clearChildren(this.#baseElement)
+    this.#currentPage = pageFunctions
+    this.#currentPage.renderer()
+  }
+}
+
+class ChangePage extends Event {
+  #pageFunctions: PageFunctions
+  constructor(pageFunctions: PageFunctions) {
+    super('changePage')
+    this.#pageFunctions = pageFunctions
+  }
+  public get pageFunctions() {
+    return this.#pageFunctions
+  }
+}
+
+export const changePage = (pageFunctions: PageFunctions) => new ChangePage(pageFunctions)
