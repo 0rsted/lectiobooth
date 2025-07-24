8f7160d971000ae35cdd205fac840c0cc03d282c
Daniel Ørsted <>
2025-07-24 15:44:34 +0200

    optimize code, add third spinner

--- src/components/spinner.ts
+++ src/components/spinner.ts
@@ -1,9 +1,12 @@
-export const spinner = () => {
-  const spinners = [spinner1, spinner2]
-  const spinner = spinners[Math.floor(Math.random() * spinners.length)]()
+export const spinner = (id?: number) => {
+  const spinners = [spinner1, spinner2, spinner3]
+  const num = (id && id < spinners.length) ? id : Math.floor(Math.random() * spinners.length)
+  const spinner = spinners[num]()
   const wrapper = document.createElement('slot')
+  const spinnerSpan = document.createElement('span')
+  spinnerSpan.className = 'loader'
   wrapper.className = 'absoluteCenter'
-  wrapper.append(spinner[0], spinner[1])
+  wrapper.append(spinner, spinnerSpan)
   return wrapper
 }
 
@@ -51,9 +54,7 @@ const spinner1 = () => {
     transform: rotate(405deg);
   }
 } `
-  const spinner = document.createElement('span')
-  spinner.className = 'loader'
-  return [styles, spinner]
+  return styles
 }
 const spinner2 = () => {
   const styles = document.createElement('style')
@@ -130,7 +131,83 @@ const spinner2 = () => {
         }
       }
    `
-  const spinner = document.createElement('span')
-  spinner.className = 'loader'
-  return [styles, spinner]
+  return styles
+}
+
+const spinner3 = () => {
+  const styles = document.createElement('style')
+  styles.textContent = `.loader {
+  box-sizing: border-box;
+  display: inline-block;
+  width: 50px;
+  height: 80px;
+  border-top: 5px solid var(--schoolPrimaryColor);
+  border-bottom: 5px solid var(--schoolPrimaryColor);
+  position: relative;
+  background: linear-gradient(#FF3D00 30px, transparent 0) no-repeat;
+  background-size: 2px 40px;
+  background-position: 50% 0px;
+  animation: spinx 5s linear infinite;
+}
+.loader:before, .loader:after {
+  content: "";
+  width: 40px;
+  left: 50%;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  transform: translatex(-50%);
+  background:  #FFFFFF40/*rgb(from var(--schoolPrimaryColor) 100% 100% 100% 40%)*/;
+  border-radius: 0 0 20px 20px;
+  background-size: 100% auto;
+  background-repeat: no-repeat;
+  background-position: 0 0px;
+  animation: lqt 5s linear infinite;
+}
+.loader:after {
+  top: auto;
+  bottom: 0;
+  border-radius: 20px 20px 0 0;
+  animation: lqb 5s linear infinite;
+}
+@keyframes lqt {
+  0%, 100% {
+    background-image: linear-gradient(var(--schoolSecondaryColor) 40px, transparent 0);
+    background-position: 0% 0px;
+  }
+  50% {
+    background-image: linear-gradient(var(--schoolSecondaryColor) 40px, transparent 0);
+    background-position: 0% 40px;
+  }
+  50.1% {
+    background-image: linear-gradient(var(--schoolSecondaryColor) 40px, transparent 0);
+    background-position: 0% -40px;
+  }
+}
+@keyframes lqb {
+  0% {
+    background-image: linear-gradient(var(--schoolSecondaryColor) 40px, transparent 0);
+    background-position: 0 40px;
+  }
+  100% {
+    background-image: linear-gradient(var(--schoolSecondaryColor) 40px, transparent 0);
+    background-position: 0 -40px;
+  }
+}
+@keyframes spinx {
+  0%, 49% {
+    transform: rotate(0deg);
+    background-position: 50% 36px;
+  }
+  51%, 98% {
+    transform: rotate(180deg);
+    background-position: 50% 4px;
+  }
+  100% {
+    transform: rotate(360deg);
+    background-position: 50% 36px;
+  }
+}
+    `
+  return styles
 }
