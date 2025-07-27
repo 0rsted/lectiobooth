b78386b268756fdf118bbe3faf2da8343c8dd7c7
Daniel Ørsted <>
2025-07-27 17:37:01 +0200

    remove debugging

--- src/lectioApi/client.ts
+++ src/lectioApi/client.ts
@@ -1,8 +1,6 @@
 import axios from "axios"
 import { Configuration } from "../functions/config"
 import * as soap from 'soap'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const debug = require('debug')('client')
 
 export const getAxios = () => {
   const ax = axios.create({
@@ -22,15 +20,10 @@ export const getAxios = () => {
 }
 
 export const getClient = async (url: string, config: Configuration) => {
-  debug('client.getClient', url, config)
   const client = await soap.createClientAsync(`${url}?singleWsdl`, {
     request: getAxios()
   })
   client.setEndpoint(url)
   client.setSecurity(new soap.BasicAuthSecurity(config.apiUser, config.apiPass))
-  client.on('request', (xml: string, eid: string) => debug('client.onRequest', xml, eid))
-  client.on('message', (message: string, eid: string) => debug('client.onMessage', message, eid))
-  client.on('soapError', (error: any, eid: string) => debug('client.onSoapError', error, eid))
-  client.on('response', (body: any, response: any, eid: string) => debug('client.onResponse', body, response, eid))
   return client
 }
